3c2f28e855ffef1b3e3b4a9ab69fc4b5a8899e52
Lucile Lefebvre <lucile.c.lefebvre@gmail.com>
2024-03-07 11:37:55 +0100

    product list component

--- src/app/(tabs)/index.tsx
+++ src/app/(tabs)/index.tsx
@@ -1,42 +1,12 @@
-import Colors from '@/constants/Colors';
-import { StyleSheet, Text, View, Image } from 'react-native';
+import { ProductListItem } from '@/components/ProductListItem';
 import products from '../../../assets/data/products'
+import { View } from 'react-native';
 
-// import EditScreenInfo from '@/components/EditScreenInfo';
-// import { Text, View } from '@/components/Themed';
-
-const product = products[0];
-
-export default function TabOneScreen() {
+export default function MenuScreen() {
   return (
-    <View style={styles.container}>
-      <Image
-        source={product.image}
-        style={styles.image}
-      />
-      <Text style={styles.title}>{product.name}</Text>
-      <Text style={styles.price}>${product.price}</Text>
+    <View>
+      <ProductListItem product={products[0]}/>
+      <ProductListItem product={products[1]}/>
     </View>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    backgroundColor: 'white',
-    padding: 10,
-    borderRadius: 20
-  },
-  image: {
-    width: '100%',
-    aspectRatio: 1
-  },
-  title: {
-    fontSize: 18,
-    fontWeight: '600',
-    marginVertical: 10
-  },
-  price: {
-    color: Colors.light.tint,
-    fontWeight: 'bold',
-  },
-});
