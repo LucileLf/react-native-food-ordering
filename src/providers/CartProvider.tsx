d1b453bd9d3527748a212f10af3244344e523f07
Lucile Lefebvre <lucile.c.lefebvre@gmail.com>
2024-03-08 13:50:46 +0100

    update cart item quantity

--- src/providers/CartProvider.tsx
+++ src/providers/CartProvider.tsx
@@ -6,11 +6,13 @@ import { randomUUID } from 'expo-crypto'
 type CartType = {
   items: CartItem[];
   addItem: (product: Product, size: CartItem['size']) => void;
+  updateQuantity: (itemId: string, amount: -1 | 1) => void;
 }
 
 export const CartContext = createContext<CartType>({
   items: [],
-  addItem: () => {}
+  addItem: () => {},
+  updateQuantity: () => {}
 });
 
 const CartProvider = ({ children }: PropsWithChildren) => { //here children refers to app stack
@@ -25,16 +27,25 @@ const CartProvider = ({ children }: PropsWithChildren) => { //here children refe
       size, // size: size
       quantity: 1,
     }
+
     setItems([newCartItem, ...items])
+
   }
 
   //update quantity
-console.log(items);
+  const updateQuantity =(itemId: string, amount: -1 | 1) => {
+    console.log(itemId, amount);
+    const updatedItems = items.map(item =>
+      item.id !== itemId ? item : {...item, quantity: item.quantity + amount}
+      );
+    setItems(updatedItems)
+  };
 
+console.log(items);
 
   return (
     <CartContext.Provider
-      value={{items: items, addItem }}
+      value={{items: items, addItem, updateQuantity }}
     >
       {children}
     </CartContext.Provider>
