6e1f015dc4d9f848d66840033a16c7eb9d9fe6ae
Lucile Lefebvre <lucile.c.lefebvre@gmail.com>
2024-03-08 17:26:00 +0100

    admin side

---
+++ src/app/(admin)/menu/_layout.tsx
@@ -0,0 +1,69 @@
+import React from "react";
+import FontAwesome from "@expo/vector-icons/FontAwesome";
+import { Link, Stack, Tabs } from "expo-router";
+import { Pressable } from "react-native";
+
+import Colors from "@/constants/Colors";
+// import { useColorScheme } from "@/components/useColorScheme";
+// import { useClientOnlyValue } from "@/components/useClientOnlyValue";
+
+// // You can explore the built-in icon families and icons on the web at https://icons.expo.fyi/
+// function TabBarIcon(props: {
+//   name: React.ComponentProps<typeof FontAwesome>["name"];
+//   color: string;
+// }) {
+//   return <FontAwesome size={20} style={{ marginBottom: -3 }} {...props} />;
+// }
+
+export default function MenuStack () {
+  // const colorScheme = useColorScheme();
+
+  return <Stack
+    screenOptions={{
+
+      }}
+    >
+   <Stack.Screen
+        name="index"
+        options={{ title: 'Menu',
+          headerRight: () => (
+            <Link href="/cart" asChild>
+              <Pressable>
+                {({ pressed }) => (
+                  <FontAwesome
+                    name="plus-square-o"
+                    size={25}
+                    color={Colors.light.tint}
+                    style={{ marginRight: 15, opacity: pressed ? 0.5 : 1 }}
+                  />
+                )}
+              </Pressable>
+            </Link>
+          ), }}
+    />
+   <Stack.Screen
+        name="[id]"
+        options={{ title: 'Menu',
+          headerRight: () => (
+            <Link href="/cart" asChild>
+              <Pressable>
+                {({ pressed }) => (
+                  <FontAwesome
+                    name="pencil"
+                    size={25}
+                    color={Colors.light.tint}
+                    style={{ marginRight: 15, opacity: pressed ? 0.5 : 1 }}
+                  />
+                )}
+              </Pressable>
+            </Link>
+          ), }}
+    />
+
+
+
+{/*
+by default, title will be name of file (index, [id])
+https://reactnavigation.org/docs/headers/#setting-the-header-title */}
+  </Stack>;
+}
