2bd6e32bcec07ab39752a92df12e6163caac7907
Lucile Lefebvre <lucile.c.lefebvre@gmail.com>
2024-03-09 20:18:54 +0100

    orders top tabs navigator

--- src/app/(admin)/orders/_layout.tsx
+++ src/app/(admin)/orders/_layout.tsx
@@ -3,10 +3,10 @@ import { Stack } from "expo-router";
 export default function OrderStack () {
   // const colorScheme = useColorScheme();
 
-  return <Stack>
-   <Stack.Screen
-        name="index"
-        options={{ title: 'Orders' }}
-      />
-  </Stack>;
+  return (
+  <Stack>
+   {/* <Stack.Screen name="index" options={{ title: 'Orders' }} /> */}
+    <Stack.Screen name="list" options={{ headerShown: false }} />
+  </Stack>
+  );
 }
